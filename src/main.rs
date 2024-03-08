074b28fc34c4bd776c3d45686ee96ad151dfb80f
Pau <raigroig@hotmail.es>
2024-03-08 22:29:10 +0100

    Add inspector-gadget binary and update UI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+#![windows_subsystem = "windows"]
 use eframe::{
-    egui::{CentralPanel, Context, RichText, ViewportBuilder},
+    egui::{CentralPanel, Context, RichText, ViewportBuilder, Image, include_image},
     App, Frame,
 };
 
@@ -28,10 +29,14 @@ impl App for MainFrame {
       ui.vertical_centered(|ui| {
 
         egui_extras::install_image_loaders(ctx);
-        ui.image("file://assets/inspector-image.png");
+        ui.add(Image::new(include_image!("../assets/inspector-image.png")).max_width(250.0));
         ui.add_space(110.0);
 
-        if (ui.button(RichText::new("Choose Excels").size(20.0))).clicked() {
+        ui.label("To select more than one file, hold Ctrl button and click on the files. Finally press Open.");
+
+        ui.add_space(20.0);
+
+        if (ui.button(RichText::new("Search Excels").size(20.0))).clicked() {
         // Show only files with the extension "xls/xlsx".
         let filter = Box::new({
             let ext_xls = Some(OsStr::new("xls"));
@@ -42,6 +47,10 @@ impl App for MainFrame {
 
         let mut dialog = FileDialog::open_file(self.opened_file.clone())
           .show_files_filter(filter)
+          .resizable(false)
+          .show_new_folder(false)
+          .show_rename(false)
+          .show_drives(false)
           .multi_select(true);
 
         dialog.open();
@@ -55,21 +64,46 @@ impl App for MainFrame {
 
         if let Some(dialog) = &mut self.open_file_dialog {
           if dialog.show(ctx).selected() {
+            self.error_message = Some("".to_string());
 
             for file in dialog.selection() {
+
+              //Check if opened is a file or a directory
+              if file.is_dir() {
+                self.error_message = Some(format!("Error: You have not selected an Excel file"));
+                println!("Error: {} is a directory", file.to_string_lossy());
+                continue;
+              }
+
               self.opened_file = Some(file.to_path_buf());
-              println!("Opened file: {:?}", file.to_path_buf());
 
               match xlsx_reader::read_xlsx_file(&file.to_path_buf()) {
-
-                Ok(results) => { 
+                
+                Ok(results) => {
                   if let Err(err) = xlsx_writer::write_xlsx_file(&results) {
-                    self.error_message = Some(format!("Error writing xlsx file: {}", err));
+
+                    let new_error = format!("\nError writing xlsx file: {}", err);
+                    self.error_message = match &self.error_message {
+                      Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                      _ => Some(new_error),
+                    };
+                  } else {
+                    let filename = file.file_name().unwrap().to_str().unwrap();
+                    let new_error = format!("\nFile {} processed successfully", filename);
+                    self.error_message = match &self.error_message {
+                      Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                      _ => Some(new_error),
+                    };
                   }
                 }
 
-                Err(err) => {
-                  self.error_message = Some(format!("Error reading xlsx file: {}", err));
+                Err(_) => {
+                  let filename = file.file_name().unwrap().to_str().unwrap();
+                  let new_error = format!("\nError reading xlsx file: {}", filename);
+                  self.error_message = match &self.error_message {
+                    Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                    _ => Some(new_error),
+                  };
                 }
 
               }  
@@ -92,7 +126,8 @@ fn main() {
     "Excel Inspector Gadget",
     eframe::NativeOptions {
       viewport: ViewportBuilder::default()
-      .with_inner_size([400.0, 600.0]),
+      .with_inner_size([400.0, 588.0])
+      .with_resizable(false),
       ..Default::default()
     },
 
