ca91ed520e726f368430e60f5fdd85a9f9ee7da3
Pau <raigroig@hotmail.es>
2024-03-09 13:15:15 +0100

    chore: change dep as we want native dialog not custom bad looking one

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
 #![windows_subsystem = "windows"]
 use eframe::{
-    egui::{CentralPanel, Context, RichText, ViewportBuilder, Image, include_image},
-    App, Frame,
+  egui::{CentralPanel, Context, RichText, ViewportBuilder, Image, include_image},
+  App, Frame,
 };
 
-use egui_file::FileDialog;
+use native_dialog::FileDialog;
+
 use egui_extras;
 
-use std::{
-    ffi::OsStr,
-    path::{Path, PathBuf},
-};
+use std::path::PathBuf;
 
 mod xlsx_reader;
 mod xlsx_writer;
   
 #[derive(Default)]
 pub struct MainFrame {
+  paths: Vec<PathBuf>,
   opened_file: Option<PathBuf>,
-  open_file_dialog: Option<FileDialog>,
   error_message: Option<String>,
 }
   
@@ -32,90 +30,68 @@ impl App for MainFrame {
         ui.add(Image::new(include_image!("../assets/inspector-image.png")).max_width(250.0));
         ui.add_space(110.0);
 
-        ui.label("To select more than one file, hold Ctrl button and click on the files. Finally press Open.");
-
-        ui.add_space(20.0);
-
         if (ui.button(RichText::new("Search Excels").size(20.0))).clicked() {
-        // Show only files with the extension "xls/xlsx".
-        let filter = Box::new({
-            let ext_xls = Some(OsStr::new("xls"));
-            let ext_xlsx = Some(OsStr::new("xlsx"));
-            move |path: &Path| -> bool { path.extension() == ext_xls 
-                || path.extension() == ext_xlsx }
-        });
 
-        let mut dialog = FileDialog::open_file(self.opened_file.clone())
-          .show_files_filter(filter)
-          .resizable(false)
-          .show_new_folder(false)
-          .show_rename(false)
-          .show_drives(false)
-          .multi_select(true);
+          // Show only files with the extension "xls/xlsx".
+          self.paths = FileDialog::new()
+          .set_location("~/Desktop")
+          .add_filter("Excel File", &["xls", "xlsx"])
+          .show_open_multiple_file()
+          .unwrap();
 
-        dialog.open();
-
-        self.open_file_dialog = Some(dialog);
         }
 
         if let Some(error_message) = &self.error_message {
           ui.label(error_message);
         }
 
-        if let Some(dialog) = &mut self.open_file_dialog {
-          if dialog.show(ctx).selected() {
-            self.error_message = Some("".to_string());
+        self.error_message = Some("".to_string());
 
-            for file in dialog.selection() {
+        for path in &self.paths {
 
-              //Check if opened is a file or a directory
-              if file.is_dir() {
-                self.error_message = Some(format!("Error: You have not selected an Excel file"));
-                println!("Error: {} is a directory", file.to_string_lossy());
-                continue;
+          //Check if opened is a file or a directory
+          if path.is_dir() {
+            self.error_message = Some(format!("Error: You have not selected an Excel file"));
+            println!("Error: {} is a directory", path.to_string_lossy());
+            continue;
+          }
+
+          self.opened_file = Some(path.to_path_buf());
+
+          match xlsx_reader::read_xlsx_file(&path.to_path_buf()) {
+            
+            Ok(results) => {
+              if let Err(err) = xlsx_writer::write_xlsx_file(&results) {
+
+                let new_error = format!("\nError writing xlsx file: {}", err);
+                self.error_message = match &self.error_message {
+                  Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                  _ => Some(new_error),
+                };
+              } else {
+                let filename = path.file_name().unwrap().to_str().unwrap();
+                let new_error = format!("\nFile {} processed successfully", filename);
+                self.error_message = match &self.error_message {
+                  Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                  _ => Some(new_error),
+                };
               }
-
-              self.opened_file = Some(file.to_path_buf());
-
-              match xlsx_reader::read_xlsx_file(&file.to_path_buf()) {
-                
-                Ok(results) => {
-                  if let Err(err) = xlsx_writer::write_xlsx_file(&results) {
-
-                    let new_error = format!("\nError writing xlsx file: {}", err);
-                    self.error_message = match &self.error_message {
-                      Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
-                      _ => Some(new_error),
-                    };
-                  } else {
-                    let filename = file.file_name().unwrap().to_str().unwrap();
-                    let new_error = format!("\nFile {} processed successfully", filename);
-                    self.error_message = match &self.error_message {
-                      Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
-                      _ => Some(new_error),
-                    };
-                  }
-                }
-
-                Err(_) => {
-                  let filename = file.file_name().unwrap().to_str().unwrap();
-                  let new_error = format!("\nError reading xlsx file: {}", filename);
-                  self.error_message = match &self.error_message {
-                    Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
-                    _ => Some(new_error),
-                  };
-                }
-
-              }  
             }
 
-          }
+            Err(_) => {
+              let filename = path.file_name().unwrap().to_str().unwrap();
+              let new_error = format!("\nError reading xlsx file: {}", filename);
+              self.error_message = match &self.error_message {
+                Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                _ => Some(new_error),
+              };
+            }
+
+          }  
         }
 
       });
-
-        
-      
+  
     });
   
   }
