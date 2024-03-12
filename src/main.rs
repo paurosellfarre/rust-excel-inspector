453b73209b7a0196bed82c68b5598b88370c4744
Pau <raigroig@hotmail.es>
2024-03-12 18:45:08 +0100

    change deps because of glow error

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-#![windows_subsystem = "windows"]
-use eframe::{
-  egui::{CentralPanel, Context, RichText, ViewportBuilder, Image, include_image},
-  App, Frame,
-};
+use iced::widget::{button, column, text,  Column};
+use iced::{Sandbox, Element, Settings};
+
 
 use native_dialog::FileDialog;
-
-use egui_extras;
 
 use std::path::PathBuf;
 
@@ -19,94 +15,123 @@ pub struct MainFrame {
   opened_file: Option<PathBuf>,
   error_message: Option<String>,
 }
-  
-impl App for MainFrame {
-  fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
-    CentralPanel::default().show(ctx, |ui| {
 
-      ui.vertical_centered(|ui| {
+#[derive(Debug, Clone)]
+pub enum Message {
+    OpenFile,
+    FileOpened,
+}
 
-        egui_extras::install_image_loaders(ctx);
-        ui.add(Image::new(include_image!("../assets/inspector-image.png")).max_width(150.0));
-        ui.add_space(25.0);
 
-        if (ui.button(RichText::new("Search Excels").size(20.0))).clicked() {
+impl Sandbox for MainFrame {
+  type Message = Message;
+
+  fn new() -> Self {
+    Self::default()
+  }
+
+  fn title(&self) -> String {
+      String::from("MainFrame")
+  }
+
+  fn update(&mut self, _message: Self::Message) {
+
+    match _message {
+      Message::OpenFile => {
 
           // Show only files with the extension "xls/xlsx".
-          self.paths = FileDialog::new()
+          let paths = FileDialog::new()
           .set_location("~/Desktop")
           .add_filter("Excel File", &["xls", "xlsx"])
           .show_open_multiple_file()
           .unwrap();
 
+          self.paths = paths;
+          self.update(Message::FileOpened);
+
         }
 
-        if let Some(error_message) = &self.error_message {
-          ui.label(error_message);
-        }
+        Message::FileOpened => {
 
-        self.error_message = Some("".to_string());
-
-        for path in &self.paths {
-
-          //Check if opened is a file or a directory
-          if path.is_dir() {
-            self.error_message = Some(format!("Error: You have not selected an Excel file"));
-            println!("Error: {} is a directory", path.to_string_lossy());
-            continue;
-          }
-
-          self.opened_file = Some(path.to_path_buf());
-
-          match xlsx_reader::read_xlsx_file(&path.to_path_buf()) {
-            
-            Ok(results) => {
-              if let Err(err) = xlsx_writer::write_xlsx_file(&results) {
-
-                let new_error = format!("\nError writing xlsx file: {}", err);
-                self.error_message = match &self.error_message {
-                  Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
-                  _ => Some(new_error),
-                };
-              } else {
+          for path in &self.paths {
+            //Check if opened is a file or a directory
+            if path.is_dir() {
+              self.error_message = Some(format!("Error: You have not selected an Excel file"));
+              println!("Error: {} is a directory", path.to_string_lossy());
+              continue;
+            }
+  
+            self.opened_file = Some(path.to_path_buf());
+  
+            match xlsx_reader::read_xlsx_file(&path.to_path_buf()) {
+              
+              Ok(results) => {
+                if let Err(err) = xlsx_writer::write_xlsx_file(&results) {
+  
+                  let new_error = format!("\nError writing xlsx file: {}", err);
+                  self.error_message = match &self.error_message {
+                    Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                    _ => Some(new_error),
+                  };
+                } else {
+                  let filename = path.file_name().unwrap().to_str().unwrap();
+                  let new_error = format!("\nFile {} processed successfully", filename);
+                  self.error_message = match &self.error_message {
+                    Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
+                    _ => Some(new_error),
+                  };
+                }
+              }
+  
+              Err(_) => {
                 let filename = path.file_name().unwrap().to_str().unwrap();
-                let new_error = format!("\nFile {} processed successfully", filename);
+                let new_error = format!("\nError reading xlsx file: {}", filename);
                 self.error_message = match &self.error_message {
                   Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
                   _ => Some(new_error),
                 };
               }
-            }
-
-            Err(_) => {
-              let filename = path.file_name().unwrap().to_str().unwrap();
-              let new_error = format!("\nError reading xlsx file: {}", filename);
-              self.error_message = match &self.error_message {
-                Some(s) if !s.is_empty() => Some(format!("{}. {}", s, new_error)),
-                _ => Some(new_error),
-              };
-            }
-
-          }  
+  
+            }   
+          }
         }
+      }
 
-      });
-  
-    });
-  
+
+    
   }
+
+  fn view(&self) -> Element<Self::Message> {
+    
+    let open_button = button::Button::new( text::Text::new("Open File"))
+      .on_press(Message::OpenFile);
+
+    let content = Column::new()
+      .push(open_button);
+
+    content.into()
+    
+  }
+  
+
+}
+
+extern "system" {
+  fn SetStdHandle(nStdHandle: u32, hHandle: *mut ()) -> i32;
 }
 
 fn main() {
-  let _ = eframe::run_native(
-    "Excel Inspector Gadget",
-    eframe::NativeOptions {
-      viewport: ViewportBuilder::default()
-      .with_inner_size([400.0, 350.0])
-      .with_resizable(false),
-      ..Default::default()
-    },
+  use std::os::windows::io::AsRawHandle;
+  //Desktop route
+  let error_log = r"C:\Users\Public\error_log.txt";
+  let f = std::fs::File::create(error_log).unwrap();
+  unsafe {
+      SetStdHandle((-12_i32) as u32, f.as_raw_handle().cast())
+  };
 
-    Box::new(|_cc| Box::new(MainFrame::default())),
-  );
+  let error = MainFrame::run(Settings::default());
+
+  if error.is_err() {
+    eprintln!("{}", error.unwrap_err());
+  }
 }
